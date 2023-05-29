e0f21b25d6390a7fca439b0490d6e8e5d68529ab
theo <theoremlguplus@gmail.com>
2023-05-29 18:17:24 +0900

    add: deploy

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,17 @@ import { Experience } from "./components/Experience";
 
 function App() {
   return (
+    
+
     <Canvas shadows camera={{ position: [0, 0.5, 4], fov: 30 }}>
       <color attach="background" args={["transparent"]} />
       <Experience />
+
+
+
     </Canvas>
   );
+
 }
 
 export default App;
